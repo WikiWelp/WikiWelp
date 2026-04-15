3950528440e1554e060a8589322895cb2152ef7e
Romualdo <romualdotomaselli6@gmail.com>
2026-04-15 15:26:57 +0200

    Fatti import nel ts di register

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+            MatStepperModule,
+            ReactiveFormsModule,
+            MatFormFieldModule,
+            MatInputModule
+          ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
