3d5892690180cf0d09954f50f1bda463a982ad36
Romualdo <romualdotomaselli6@gmail.com>
2026-04-15 15:37:17 +0200

    Implementato l'html di register con i 3 step

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,5 +18,27 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    email: ['', [Validators.required]],
+  });
+
+  secondFormGroup = this._formBuilder.group({
+    password: ['', Validators.required],
+  });
+
+    submitRegistration() {
+    const email = this.firstFormGroup.get('email')?.value;
+    const password = this.secondFormGroup.get('password')?.value;
+
+    console.log({
+      email,
+      password,
+    });
+
+    this.router.navigate(['/']);
+  }
 }
