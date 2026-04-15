015ccc8b35dbcbb536b993e11ef1539d789bd3ec
Romualdo <romualdotomaselli6@gmail.com>
2026-04-15 14:51:35 +0200

    Implementate le funzioni setLogin e isLoggedIn in  servizio.service.ts

--- frontend/src/app/servizio.service.ts
+++ frontend/src/app/servizio.service.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ServizioService {
+  login: boolean = false;
 
+  setLogin(status: boolean){
+    this.login=status;
+  }
+
+  isLoggedIn(): boolean{
+    return this.login
+  }
   constructor() { }
 }
