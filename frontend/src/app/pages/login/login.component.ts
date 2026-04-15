037cb853d615092e5d54a26e143a156c185702c1
Romualdo <romualdotomaselli6@gmail.com>
2026-04-15 15:09:52 +0200

    Aggiunti import nel login.ts

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+            MatInputModule,
+            MatFormFieldModule,
+            ReactiveFormsModule
+          ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
