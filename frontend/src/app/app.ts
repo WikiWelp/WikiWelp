4a425664f19da085535f482aca7769405183b06e
Giovanni Ricca <giovanniricca@proton.me>
2026-04-22 14:08:43 +0200

    frontend: Remove `ping` result
    Change-Id: I4d4f195c83faefbd390f879333cf6a62938edeaf

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,29 +1,16 @@
 import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-            RouterModule
-            ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  title="Wiki"
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
-
-  protected pingResult = signal('');
-
-  constructor() {
-    this.http.get(`${this.baseUrl}/api/ping`, { responseType: 'text' })
-      .subscribe({
-        next: (text) => this.pingResult.set(text),
-        error: () => this.pingResult.set('error')
-      });
-  }
 }
