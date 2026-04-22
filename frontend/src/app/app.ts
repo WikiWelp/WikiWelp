3fd05a2e6ee3a77b0090b97965c236c72e2f1981
Giovanni Ricca <giovanniricca@proton.me>
2026-04-22 14:04:54 +0200

    Revert "frontend: Add `createUser` and `findUser` functions"
    Reason: It will conflict with the frontend branch.
    
    This reverts commit 384e322380de262d4967289db07bb95abc8c0f24.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,12 +3,6 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
-interface UserDAO {
-  id: number;
-  email: string;
-  password: string;
-}
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -21,35 +15,11 @@ export class App {
 
   protected pingResult = signal('');
 
-  protected userCreateEmail = '';
-  protected userCreatePassword = '';
-  protected createUserResult = signal('');
-
-  protected userFindEmail = '';
-  protected findUserResult = signal('');
-
   constructor() {
     this.http.get(`${this.baseUrl}/api/ping`, { responseType: 'text' })
       .subscribe({
         next: (text) => this.pingResult.set(text),
         error: () => this.pingResult.set('error')
-      });
-  }
-
-  protected createUser() {
-    const user: UserDAO = { id: 0, email: this.userCreateEmail, password: this.userCreatePassword};
-    this.http.post<UserDAO>(`${this.baseUrl}/api/user/create`, user)
-      .subscribe({
-        next: () => this.createUserResult.set('User created!'),
-        error: (e) => this.createUserResult.set(e.status)
-      });
-  }
-
-  protected findUser() {
-    this.http.get<UserDAO>(`${this.baseUrl}/api/user/${this.userFindEmail}`)
-      .subscribe({
-        next: (u) => this.findUserResult.set(`ID: ${u.id}, Email: ${u.email}, Password: ${u.password}`),
-        error: (e) => this.findUserResult.set(e.status)
       });
   }
 }
