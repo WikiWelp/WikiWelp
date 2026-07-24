e45afea213da8e8e7ee39eecd5e3aa1373bb16cb
Romualdo <romualdotomaselli6@gmail.com>
2026-07-24 17:22:03 +0200

    Aggiunti import in edit.css

--- frontend/src/app/pages/edit/edit.component.ts
+++ frontend/src/app/pages/edit/edit.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RichTextEditorModule, ToolbarService, LinkService, ImageService, HtmlEditorService  } from '@syncfusion/ej2-angular-richtexteditor';
 
+
 @Component({
   selector: 'app-edit',
   standalone: true,
