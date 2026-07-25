754463b41c778ee9cafff8b1fac97f57042eb9f8
Romualdo <romualdotomaselli6@gmail.com>
2026-07-25 20:09:26 +0200

    Iinserita la licenza provvisoria (me ne daranno una vera e propria dopo due giorni) per utilizzare l'editor.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Ngo9BigBOggjHTQxAR8/V1JAaF5cX2pCfFN0Q35Zd0NwdUVHY1ZVRXxeQ1ZhSXxVdkJhXH5ddHVUR2VUUE19XEY=');
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
