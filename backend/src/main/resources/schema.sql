eef555f6558e982212e873ac26cae380ff3a89d1
Giovanni Ricca <giovanniricca@proton.me>
2026-08-24 18:53:56 +0200

    backend: Add schema for pages
    Change-Id: If9ee2979645fd87255f1fd6e6f058eab70bd9d28

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS users
     email     VARCHAR(255)  UNIQUE NOT NULL,
     password  VARCHAR(255)  NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS pages
+(
+    id        SERIAL        PRIMARY KEY,
+    title     VARCHAR(255)  UNIQUE NOT NULL,
+    content   TEXT          NOT NULL
+);
