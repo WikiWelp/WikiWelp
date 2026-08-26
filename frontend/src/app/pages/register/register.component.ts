1d661efe0119884e7622ae271d5a2ffbecbbbbb5
Romualdo <romualdotomaselli6@gmail.com>
2026-08-26 17:12:03 +0200

    Prova register responsive

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, DestroyRef} from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
 import { ServizioService } from '../../services/servizio.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-register',
@@ -23,6 +25,10 @@ export class RegisterComponent {
   ) {}
 
   private _formBuilder = inject(FormBuilder);
+  private breakpointObserver=inject(BreakpointObserver);
+  private destroyRef=inject(DestroyRef);
+
+  isMobile: boolean =false;
 
   firstFormGroup = this._formBuilder.group({
     email: ['', [Validators.required]],
@@ -31,6 +37,15 @@ export class RegisterComponent {
   secondFormGroup = this._formBuilder.group({
     password: ['', Validators.required],
   });
+
+  ngOnInit(): void {
+    this.breakpointObserver
+    .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
+    .pipe(takeUntilDestroyed(this.destroyRef)) // Evita perdite di memoria alla chiusura del componente
+    .subscribe(result => {
+      this.isMobile = result.matches;
+    });
+  }
 
   submitRegistration() {
     const email = this.firstFormGroup.get('email')?.value;
