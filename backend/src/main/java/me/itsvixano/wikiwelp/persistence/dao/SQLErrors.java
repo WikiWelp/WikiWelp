2bdf412b6c707d04bf0b95f7a9669a6b3b8b1d48
Giovanni Ricca <giovanniricca@proton.me>
2026-09-04 10:31:54 +0200

    backend: Make SQL_DUPLICATE_KEY final
    Change-Id: Ia06b3ba5573fd9e80d1a0411b73784f3aa75b8a3

--- backend/src/main/java/me/itsvixano/wikiwelp/persistence/dao/SQLErrors.java
+++ backend/src/main/java/me/itsvixano/wikiwelp/persistence/dao/SQLErrors.java
@@ -1,6 +1,5 @@
 package me.itsvixano.wikiwelp.persistence.dao;
 
-// Should have been an enum, but in java they suck :(
 public class SQLErrors {
-    public static String SQL_DUPLICATE_KEY = "23505";
+    public static final String SQL_DUPLICATE_KEY = "23505";
 }
